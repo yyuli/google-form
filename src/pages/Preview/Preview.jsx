873623c5ef0ea458b3df437687a757357db6a3a7
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-07-22 12:56:44 +0900

    Fix: Preview Style 분리

--- src/pages/Preview/Preview.jsx
+++ src/pages/Preview/Preview.jsx
@@ -1,136 +1,50 @@
 import React, { useState } from "react";
 import TitleBox from "../../components/TitleBox/TitleBox";
-import QuestionList from "../../components/QuestionList/QuestionList";
 import { useSelector } from "react-redux";
-import square from "../../assets/images/square.svg";
-import squareChecked from "../../assets/images/square-checked.svg";
-import circle from "../../assets/images/circle.svg";
-import circleChecked from "../../assets/images/circle-checked.svg";
 import {
   QuestionItemWrap,
   QuestionItemDragDiv,
   QuestionItemTitle,
   QuestionItemLi,
-  QuestionItemP,
   QuestionListWrapDiv,
   QuestionItemRequiredSpan,
 } from "../../components/QuestionItem/QuestionItemStyle";
+import { QuestionTypeSelect } from "../../components/QuestionBox/QuestionBoxStyle";
 import {
-  QuestionDragBtn,
-  QuestionListEtcDiv,
-  QuestionListInput,
-  QuestionListShortInput,
-  QuestionListLongInput,
-  QuestionListNumberSpan,
-  QuestionTypeSelect,
-} from "../../components/QuestionBox/QuestionBoxStyle";
-import styled, { css } from "styled-components";
-import {
-  ExpandBorderBottom,
-  CommonSpan,
-} from "../../components/TitleBox/TitleBoxStyle";
-
-const PreviewEtcDiv = styled(QuestionListEtcDiv)`
-  display: flex;
-  align-items: center;
-`;
-const PreviewEtcLabel = styled.label`
-  display: block;
-  font-size: 14px;
-  width: 32px;
-  margin-right: 20px;
-`;
-const AnimatedPreviewEtcSpan = styled.span`
-  ${CommonSpan}
-  bottom: 0;
-`;
-const AnimatePreviewEtcDiv = styled.div`
-  position: relative;
-  width: 593px;
-  height: 24px;
-`;
-const PreviewEtcInput = styled(QuestionListInput)`
-  border-bottom: 1px solid #0000001f;
-  font-size: 16px;
-  height: 24px;
-  &:focus + ${AnimatedPreviewEtcSpan} {
-    animation: ${ExpandBorderBottom} 0.4s forwards;
-  }
-`;
-const CommonCustom = css`
-  appearance: none;
-  width: 20px;
-  height: 20px;
-  margin: 0;
-  vertical-align: middle;
-  transition: all 0.3s;
-`;
-const PreviewCustomInput = styled.input`
-  ${CommonCustom}
-  background: url(${square}) no-repeat center / 90%;
-  &:checked {
-    background-image: url(${squareChecked});
-  }
-`;
-const PreviewCustomLabel = styled.label`
-  font-size: 14px;
-  margin-left: 10px;
-`;
-const PreviewCustomRadio = styled.input`
-  ${CommonCustom}
-  background: url(${circle}) no-repeat center / 90%;
-  &:checked {
-    background-image: url(${circleChecked});
-  }
-`;
-const AnimatedPreviewShortSpan = styled.span`
-  ${CommonSpan}
-  bottom: 0;
-`;
-const AnimatePreviewShortDiv = styled.div`
-  position: relative;
-  width: 360px;
-  height: 24px;
-`;
-const PreviewShortInput = styled(QuestionListShortInput)`
-  width: 100%;
-  &:focus + ${AnimatedPreviewShortSpan} {
-    animation: ${ExpandBorderBottom} 0.4s forwards;
-  }
-`;
-const AnimatedPreviewLongSpan = styled.span`
-  ${CommonSpan}
-  bottom: 0;
-`;
-const AnimatePreviewLongDiv = styled.div`
-  position: relative;
-  width: 612px;
-  height: 24px;
-`;
-const PreviewLongInput = styled(QuestionListLongInput)`
-  width: 100%;
-  &:focus + ${AnimatedPreviewLongSpan} {
-    animation: ${ExpandBorderBottom} 0.4s forwards;
-  }
-`;
+  PreviewEtcDiv,
+  PreviewEtcLabel,
+  AnimatedPreviewEtcSpan,
+  AnimatePreviewEtcDiv,
+  PreviewEtcInput,
+  PreviewCustomInput,
+  PreviewCustomLabel,
+  PreviewCustomRadio,
+  AnimatedPreviewShortSpan,
+  AnimatePreviewShortDiv,
+  PreviewShortInput,
+  AnimatedPreviewLongSpan,
+  AnimatePreviewLongDiv,
+  PreviewLongInput,
+  BtnWrap,
+  SubmitBtn,
+  ResetBtn,
+} from "./PreviewStyle";
 
 export default function Preview() {
   const questionListItem = useSelector((state) => state.questionListItem.value);
-  console.log(questionListItem);
   const [isActive, setIsActive] = useState(false);
   const [selectedOption, setSelectedOption] = useState("선택");
   const [checked, setChecked] = useState(false);
   const handleCheckboxEtcClick = () => {
     setChecked(!checked);
   };
-  const [checkedTemp, setCheckedTemp] = useState(false);
-  const handleCheckboxEtcClickTemp = () => {
-    setCheckedTemp(!checkedTemp);
+  const [checkedRadio, setCheckedRadio] = useState(false);
+  const handleCheckboxEtcClickRadio = () => {
+    setCheckedRadio(!checkedRadio);
   };
   const randomId = () => {
     return "_" + Math.random().toString(36).substring(2, 9);
   };
-  console.log(randomId());
 
   return (
     <>
@@ -176,19 +90,19 @@ export default function Preview() {
                   <PreviewCustomRadio
                     type="checkbox"
                     id="radio-etc"
-                    checked={checkedTemp}
+                    checked={checkedRadio}
                   />
                   <PreviewEtcDiv>
                     <PreviewEtcLabel
                       htmlFor="radio-etc"
-                      onClick={handleCheckboxEtcClickTemp}
+                      onClick={handleCheckboxEtcClickRadio}
                     >
                       기타:
                     </PreviewEtcLabel>
                     <AnimatePreviewEtcDiv>
                       <PreviewEtcInput
                         type="text"
-                        onClick={handleCheckboxEtcClickTemp}
+                        onClick={handleCheckboxEtcClickRadio}
                       />
                       <AnimatedPreviewEtcSpan />
                     </AnimatePreviewEtcDiv>
@@ -257,6 +171,10 @@ export default function Preview() {
             </QuestionItemWrap>
           );
         })}
+      <BtnWrap>
+        <SubmitBtn type="submit">제출</SubmitBtn>
+        <ResetBtn type="button">양식 지우기</ResetBtn>
+      </BtnWrap>
     </>
   );
 }
---
+++ src/pages/Preview/PreviewStyle.jsx
@@ -0,0 +1,144 @@
+import styled, { css } from "styled-components";
+import square from "../../assets/images/square.svg";
+import squareChecked from "../../assets/images/square-checked.svg";
+import circle from "../../assets/images/circle.svg";
+import circleChecked from "../../assets/images/circle-checked.svg";
+import {
+  QuestionListEtcDiv,
+  QuestionListInput,
+  QuestionListShortInput,
+  QuestionListLongInput,
+} from "../../components/QuestionBox/QuestionBoxStyle";
+import {
+  ExpandBorderBottom,
+  CommonSpan,
+} from "../../components/TitleBox/TitleBoxStyle";
+
+const PreviewEtcDiv = styled(QuestionListEtcDiv)`
+  display: flex;
+  align-items: center;
+`;
+const PreviewEtcLabel = styled.label`
+  display: block;
+  font-size: 14px;
+  width: 32px;
+  margin-right: 20px;
+`;
+const AnimatedPreviewEtcSpan = styled.span`
+  ${CommonSpan}
+  bottom: 0;
+`;
+const AnimatePreviewEtcDiv = styled.div`
+  position: relative;
+  width: 593px;
+  height: 24px;
+`;
+const PreviewEtcInput = styled(QuestionListInput)`
+  border-bottom: 1px solid #0000001f;
+  font-size: 16px;
+  height: 24px;
+  &:focus + ${AnimatedPreviewEtcSpan} {
+    animation: ${ExpandBorderBottom} 0.4s forwards;
+  }
+`;
+const CommonCustom = css`
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  margin: 0;
+  vertical-align: middle;
+  transition: all 0.3s;
+`;
+const PreviewCustomInput = styled.input`
+  ${CommonCustom}
+  background: url(${square}) no-repeat center / 90%;
+  &:checked {
+    background-image: url(${squareChecked});
+  }
+`;
+const PreviewCustomLabel = styled.label`
+  font-size: 14px;
+  margin-left: 10px;
+`;
+const PreviewCustomRadio = styled.input`
+  ${CommonCustom}
+  background: url(${circle}) no-repeat center / 90%;
+  &:checked {
+    background-image: url(${circleChecked});
+  }
+`;
+const AnimatedPreviewShortSpan = styled.span`
+  ${CommonSpan}
+  bottom: 0;
+`;
+const AnimatePreviewShortDiv = styled.div`
+  position: relative;
+  width: 360px;
+  height: 24px;
+`;
+const PreviewShortInput = styled(QuestionListShortInput)`
+  width: 100%;
+  &:focus + ${AnimatedPreviewShortSpan} {
+    animation: ${ExpandBorderBottom} 0.4s forwards;
+  }
+`;
+const AnimatedPreviewLongSpan = styled.span`
+  ${CommonSpan}
+  bottom: 0;
+`;
+const AnimatePreviewLongDiv = styled.div`
+  position: relative;
+  width: 612px;
+  height: 24px;
+`;
+const PreviewLongInput = styled(QuestionListLongInput)`
+  width: 100%;
+  &:focus + ${AnimatedPreviewLongSpan} {
+    animation: ${ExpandBorderBottom} 0.4s forwards;
+  }
+`;
+const BtnWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const SubmitBtn = styled.button`
+  width: 73px;
+  height: 36px;
+  background-color: #673ab7;
+  border-radius: 4px;
+  font-size: 14px;
+  color: #fff;
+  cursor: pointer;
+  padding: 0 24px;
+`;
+const ResetBtn = styled.button`
+  height: 36px;
+  color: #673ab7;
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 14px;
+  padding: 0 8px;
+  &:hover {
+    background-color: #e7e3f5;
+  }
+`;
+
+export {
+  PreviewEtcDiv,
+  PreviewEtcLabel,
+  AnimatedPreviewEtcSpan,
+  AnimatePreviewEtcDiv,
+  PreviewEtcInput,
+  PreviewCustomInput,
+  PreviewCustomLabel,
+  PreviewCustomRadio,
+  AnimatedPreviewShortSpan,
+  AnimatePreviewShortDiv,
+  PreviewShortInput,
+  AnimatedPreviewLongSpan,
+  AnimatePreviewLongDiv,
+  PreviewLongInput,
+  BtnWrap,
+  SubmitBtn,
+  ResetBtn,
+};
