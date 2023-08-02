2d31b04e7969dfd2366a1000aa1c1c25fdfd348f
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-08-02 16:12:33 +0900

    Style: Modal background-color 수정

--- src/components/Modal/ModalStyle.jsx
+++ src/components/Modal/ModalStyle.jsx
@@ -10,7 +10,7 @@ const ModalBackground = styled.article`
   width: 100%;
   height: 100%;
   z-index: 999;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.5);
 `;
 
 const ModalWrapper = styled.div`
